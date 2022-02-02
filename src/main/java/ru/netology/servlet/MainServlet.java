6005a30cc7ab03e20719f384609fe24f9add0e89
Viktor Loskutov <vikandloskutov@gmail.com>
2022-02-03 00:07:39 +0300

    Initial commit

---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,59 @@
+package ru.netology.servlet;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.controller.PostController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+  public static final String GET_METHOD = "GET";
+  public static final String POST_METHOD = "POST";
+  public static final String DELETE_METHOD = "DELETE";
+  private PostController controller;
+
+  @Override
+  public void init() {
+    final var context = new AnnotationConfigApplicationContext("ru.netology");
+    controller = context.getBean(PostController.class);
+  }
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) {
+    // если деплоились в root context, то достаточно этого
+    try {
+      final var path = req.getRequestURI();
+      final var method = req.getMethod();
+      // primitive routing
+      if (method.equals(GET_METHOD) && path.equals("/api/posts")) {
+        controller.all(resp);
+        return;
+      }
+      if (method.equals(GET_METHOD) && path.matches("/api/posts/\\d+")) {
+        // easy way
+        final var id = getIdFromUrl(path);
+        controller.getById(id, resp);
+        return;
+      }
+      if (method.equals(POST_METHOD) && path.equals("/api/posts")) {
+        controller.save(req.getReader(), resp);
+        return;
+      }
+      if (method.equals(DELETE_METHOD) && path.matches("/api/posts/\\d+")) {
+        // easy way
+        final var id = getIdFromUrl(path);
+        controller.removeById(id, resp);
+        return;
+      }
+      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    } catch (Exception e) {
+      e.printStackTrace();
+      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  private long getIdFromUrl(String path) {
+    return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+  }
+}
